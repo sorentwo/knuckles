fc4f9836961a9ae9928f1fb48a0a23b7eccb0fef
Parker Selbert <parker@sorentwo.com>
2015-05-20 23:53:43 +0200

    Define cache_key semantics for node object

--- lib/knuckles/node.rb
+++ lib/knuckles/node.rb
@@ -6,8 +6,28 @@ module Knuckles
     def initialize(object, serializer: nil, dependencies: nil, serialized: nil)
       @object       = object
       @serializer   = serializer
-      @dependencies = dependencies
+      @dependencies = dependencies || []
       @serialized   = serialized
+    end
+
+    def cache_key
+      [serializer_cache_key, dependency_cache_key].compact
+    end
+
+    private
+
+    def serializer_cache_key
+      if serializer.respond_to?(:cache_key)
+        serializer.cache_key
+      else
+        object.cache_key
+      end
+    end
+
+    def dependency_cache_key
+      if dependencies.any?
+        dependencies.max_by { |dep| dep.updated_at }.cache_key
+      end
     end
   end
 end
