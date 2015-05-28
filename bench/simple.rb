0c623166046297e64ef0240c4ca1c72ed86ed678
Parker Selbert <parker@sorentwo.com>
2015-05-28 22:13:24 +0200

    Restore post serializer to simple benchmark

--- bench/simple.rb
+++ bench/simple.rb
@@ -4,6 +4,16 @@ Knuckles.cache = ActiveSupport::Cache::MemoryStore.new
 
 Post = Struct.new(:id, :title, :created_at)
 
+class PostSerializer < Knuckles::Serializer
+  root :posts
+
+  attributes :id, :title, :created_at
+
+  def cache_key
+    "posts/#{id}"
+  end
+end
+
 pipeline = Knuckles::Pipeline.new
 models   = 100.times.map { |i| Post.new(i, "title", Date.new) }
 
