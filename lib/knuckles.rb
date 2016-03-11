39c238d6a94dfeec6d40a0a1f12c53bf5ca633fd
Parker Selbert <parker@sorentwo.com>
2016-03-11 08:10:55 -0600

    Add rendering and string rendering to top level

--- lib/knuckles.rb
+++ lib/knuckles.rb
@@ -1,17 +1,32 @@
 require "json"
+require "set"
 
 module Knuckles
   extend self
 
   def render(objects, view, options = {})
-    objects.each_with_object(backed_hash) do |object, memo|
+    objects.each_with_object(set_backed_hash) do |object, memo|
       memo[view.root] << view.data(object, options)
+
+      view.relations(object, options).each do |root, data|
+        memo[root] += data
+      end
     end
+  end
+
+  def render_to_string(object, view, options = {})
+    JSON.dump(set_backed_to_array(render(object, view, options)))
   end
 
   private
 
-  def backed_hash
-    Hash.new { |hash, key| hash[key] = [] }
+  def set_backed_hash
+    Hash.new { |hash, key| hash[key] = Set.new }
+  end
+
+  def set_backed_to_array(hash)
+    hash.each_with_object({}) do |(root, set), memo|
+      memo[root] = set.to_a
+    end
   end
 end
--- spec/knuckles_spec.rb
+++ spec/knuckles_spec.rb
@@ -8,11 +8,11 @@ PostView = Module.new do
   extend Knuckles::View
 
   def self.root
-    "posts"
+    :posts
   end
 
   def self.data(post, _)
-    {id: post.id, title: post.title}
+    {id: post.id, title: post.title, tag_ids: post.tags.map(&:id)}
   end
 
   def self.relations(post, _)
@@ -24,7 +24,7 @@ TagView = Module.new do
   extend Knuckles::View
 
   def self.root
-    "tags"
+    :tags
   end
 
   def self.data(tag, _)
@@ -33,13 +33,33 @@ TagView = Module.new do
 end
 
 RSpec.describe Knuckles do
+  let(:tag_a)  { Tag.new(1, "alpha") }
+  let(:tag_b)  { Tag.new(2, "gamma") }
+  let(:post_a) { Post.new(1, "hello", [tag_a, tag_b]) }
+  let(:post_b) { Post.new(2, "there", [tag_a]) }
+
   describe "#render" do
     it "serializes a collection of objects" do
-      posts = [Post.new(1, "hello", []), Post.new(2, "there", [])]
-
-      rendered = Knuckles.render(posts, PostView)
+      rendered = Knuckles.render([post_a, post_b], PostView)
 
       expect(rendered).to eq(
+        posts: Set.new([
+          {id: 1, title: "hello", tag_ids: [1, 2]},
+          {id: 2, title: "there", tag_ids: [1]}
+        ]),
+        tags: Set.new([
+          {id: 1, name: "alpha"},
+          {id: 2, name: "gamma"}
+        ])
+      )
+    end
+  end
+
+  describe "#render_to_string" do
+    it "dumps the serialized collection to a string" do
+      rendered = Knuckles.render_to_string([post_a, post_b], PostView)
+
+      expect(rendered).to eq(JSON.dump(
         posts: [
           {id: 1, title: "hello", tag_ids: [1, 2]},
           {id: 2, title: "there", tag_ids: [1]}
@@ -48,7 +68,7 @@ RSpec.describe Knuckles do
           {id: 1, name: "alpha"},
           {id: 2, name: "gamma"}
         ]
-      )
+      ))
     end
   end
 end
