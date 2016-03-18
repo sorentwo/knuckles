c8d61faac51070d3b70e2781cc40aeb2d18e4b82
Parker Selbert <parker@sorentwo.com>
2016-03-18 08:21:20 -0500

    Leverage write_multi for cache that provides it

--- lib/knuckles/writer.rb
+++ lib/knuckles/writer.rb
@@ -7,15 +7,33 @@ module Knuckles
     end
 
     def call(objects, _)
-      objects.each do |hash|
-        write(hash[:key], hash[:result]) unless hash[:cached?]
+      if cache.respond_to?(:write_multi)
+        write_multi(objects)
+      else
+        write_each(objects)
       end
     end
 
     private
 
-    def write(key, value)
-      Knuckles.cache.write(key, value)
+    def cache
+      Knuckles.cache
+    end
+
+    def write_each(objects)
+      objects.each do |hash|
+        cache.write(hash[:key], hash[:result]) unless hash[:cached?]
+      end
+    end
+
+    def write_multi(objects)
+      writable = objects.each_with_object({}) do |hash, memo|
+        next if hash[:cached?]
+
+        memo[hash[:key]] = hash[:result]
+      end
+
+      cache.write_multi(writable) if writable.any?
     end
   end
 end
--- spec/knuckles/writer_spec.rb
+++ spec/knuckles/writer_spec.rb
@@ -1,4 +1,14 @@
 RSpec.describe Knuckles::Writer do
+  class FakeCache
+    def write_multi(*)
+      true
+    end
+  end
+
+  after do
+    Knuckles.reset!
+  end
+
   describe ".call" do
     it "writes all un-cached values" do
       objects = [
@@ -10,6 +20,19 @@ RSpec.describe Knuckles::Writer do
 
       expect(Knuckles.cache.read("t/1/1")).to be_nil
       expect(Knuckles.cache.read("t/2/2")).not_to be_nil
+    end
+
+    it "leverages write multi for caches that support it" do
+      objects = [
+        {key: "t/1/1", cached?: false, result: {tags: []}},
+        {key: "t/2/1", cached?: true, result: {tags: []}}
+      ]
+
+      Knuckles.cache = FakeCache.new
+
+      expect(Knuckles.cache).to receive(:write_multi) { true }
+
+      Knuckles::Writer.call(objects, {})
     end
   end
 end
