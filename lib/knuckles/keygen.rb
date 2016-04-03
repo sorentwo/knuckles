68c71a37d0ef3efb85de1b38853691d04a434814
Parker Selbert <parker@sorentwo.com>
2016-04-03 13:51:01 -0500

    Add documentation for keygen module

--- lib/knuckles/keygen.rb
+++ lib/knuckles/keygen.rb
@@ -1,9 +1,31 @@
 # frozen_string_literal: true
 
 module Knuckles
+  # The Keygen module provides simple cache key generation. The global keygen
+  # strategy can be changed at the top level by configuring `Knuckles.keygen`.
+  #
+  # Any object that responds to `expand_key` with a single argument can be
+  # used instead.
+  #
+  # @example Change global keygen stragey
+  #
+  #   Knuckles.keygen = MyCustomKeygen.new
+  #
   module Keygen
     extend self
 
+    # Calculates a cache key for the given object. It first attempts to use
+    # the object's `cache_key` method (present on `ActiveRecord` models). It
+    # falls back to combining the object's `id` and `updated_at` values.
+    #
+    # @param [Object] Object to caclculate key for
+    #
+    # @return [String] Computed cache key
+    #
+    # @example
+    #
+    #   Knuckles::Keygen.expand_key(model) #=> "MyModel/1/1234567890"
+    #
     def expand_key(object)
       if object.respond_to?(:cache_key)
         object.cache_key
