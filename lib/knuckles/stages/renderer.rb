5447b0cbc0ed6b5842eb5ffd1b3213f6f5bcb070
Parker Selbert <parker@sorentwo.com>
2016-06-16 17:55:22 -0500

    Document the renderer stage

--- lib/knuckles/stages/renderer.rb
+++ lib/knuckles/stages/renderer.rb
@@ -2,9 +2,33 @@
 
 module Knuckles
   module Stages
+    # After un-cached models have been hydrated they can be rendered. Rendering is
+    # synonymous with converting a model to a hash, like calling `as_json` on an
+    # `ActiveRecord` model. Knuckles provides a minimal (but fast) view module that
+    # can be used with the rendering step. Alternatively, if you're migrating from
+    # `ActiveModelSerializers` you can pass in an AMS class instead.
     module Renderer
       extend self
 
+      # Serialize all un-cached objects into hashes.
+      #
+      # @param [Enumerable] prepared The prepared collection to be rendered
+      # @option [Module] :view A `Knuckles::View` compliant module,
+      #   it will be passed the object and any options. Alternately,
+      #   a class compatible with the `ActiveModelSerializers` API.
+      #
+      # @example Using a Knuckles::View
+      #
+      #     module PostView
+      #       extend Knuckles::View
+      #
+      #       def self.data(post, _options)
+      #         {id: post.id, name: post.name}
+      #       end
+      #     end
+      #
+      #     pipeline.call(models, view: PostView)
+      #
       def call(objects, options)
         view = options.fetch(:view)
 
